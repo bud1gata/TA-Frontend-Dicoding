af0997764127f32ce96f789816e3be372e6e247c
bud1gata <bud1gata@gmail.com>
2024-08-06 21:21:44 +0700

    finshing and wrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { TitleHeader } from "./components/header.js";
 import { InputForm } from "./components/input.js";
-import { NoteItem } from "./components/note-item.js";
 import { ShowList } from "./components/showList.js";
 import css from "./styles/styles.css";
 import { getNotes } from "../src/api/requestGet.js"
